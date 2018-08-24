4005387287701c8ffea2b07dd2d0474c59796b40
Sabari <37924763+Sabari70@users.noreply.github.com>
2018-08-24 17:06:57 +0530

    Update TextFiletoHashMap.py

--- TextFiletoHashMap.py
+++ TextFiletoHashMap.py
@@ -1,10 +1,15 @@
 #Reading contents from Text file and store it in Hashmap then sort in Ascending order with Hash keys
 
-with open('demofile.txt',"r") as f:
-    hash = {}
+from operator import itemgetter 
+
+hash = {}
+with open('demofile.txt') as f:
     for line in f:
-         key,value = line.strip().split('.',1)
-         hash[key] = int(value)
-         value_sort = sorted(hash.value())
-         f.write(value_sort)
-         
+        key, value = line.strip().split('|', 1)
+        hash[key] = int(value)
+
+#for key, value in sorted(hash.items(), key=itemgetter(1)):
+   # print '{0}.{1}'.format(key, value)
+with open('example.txt', 'w') as f:
+    for key, value in sorted(hash.items(), key=itemgetter(1)):
+        f.write('{0}|{1}\n'.format(key, value))
